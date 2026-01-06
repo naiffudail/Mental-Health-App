457f3ca96ba9a8843922a9ab753c617135bfad2d
naiffudail <naif.fudail@gmail.com>
2026-01-07 06:05:04 +0800

    login interface

---
+++ app/src/main/java/com/example/healthapp/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.healthapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val adminButton = findViewById<Button>(R.id.admin_button)
+        adminButton.setOnClickListener {
+            val intent = Intent(this, signup::class.java)
+            startActivity(intent)
+        }
+
+        val userButton = findViewById<Button>(R.id.user_button)
+        userButton.setOnClickListener {
+            val intent = Intent(this, signup::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/healthapp/MainActivity.kt
+++ app/src/main/java/com/example/healthapp/MainActivity.kt
@@ -12,8 +12,8 @@ class MainActivity : AppCompatActivity() {
 
         var button3 = findViewById<Button>(R.id.button3)
         button3.setOnClickListener{
-            val intent1 = Intent(this,signup::class.java)
-            startActivity(intent1)
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
 
 
