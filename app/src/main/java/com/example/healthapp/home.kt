6e97b35ffe77e6a93e106f7d66170e46f1a034e3
ikhwan0204 <khairul6347@gmail.com>
2026-01-07 22:04:14 +0800

    music ui

--- app/src/main/java/com/example/healthapp/home.kt
+++ app/src/main/java/com/example/healthapp/home.kt
@@ -1,11 +1,71 @@
 package com.example.healthapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_music -> {
+                val intent = Intent(this, MusicActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_home -> {
+                // TODO: Handle home click
+            }
+            R.id.nav_diary -> {
+                // TODO: Handle diary click
+            }
+            R.id.nav_profile -> {
+                // TODO: Handle profile click
+            }
+            R.id.nav_community -> {
+                // TODO: Handle community click
+            }
+            R.id.nav_chat -> {
+                // TODO: Handle chat click
+            }
+            R.id.nav_progress -> {
+                // TODO: Handle progress click
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
